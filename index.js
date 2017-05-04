67699a0cd193bc427a9d800ce10e3e8a19b61b9d
Brian Reed <me@iambrian.com>
2017-05-03 23:55:44 -0400

    work it

--- index.js
+++ index.js
@@ -2,4 +2,19 @@
  * Nothing to see here
  */
 
-console.log(process.env);
+const cool = require('cool-ascii-faces');
+const express = require('express');
+const app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function (request, response) {
+    response.send(cool());
+});
+
+app.listen(app.get('port'), function () {
+    console.log('Node app is running on port', app.get('port'));
+});
+
